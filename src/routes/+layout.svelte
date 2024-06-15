d3b94ff37f87e1987227ba36f4b2225decd49ece
Nick Ng <nick-ng@users.noreply.github.com>
2024-06-15 19:29:54 +1200

    feat: add an anki-stuff page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
 	import '../app.css';
 </script>
 
-<div class="flex flex-row mx-2 my-2">
+<div class="flex flex-row mx-2 my-2 gap-2">
 	<a class="button-default" href="/op-corners">Old Pochman Corners</a>
 	<a class="button-default" href="/corner-drawer">Corner Drawer</a>
+	<a class="button-default" href="/anki-stuff">Anki Stuff</a>
 </div>
 <div class="mx-1">
 	<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<div class="max-w-[130ch] mx-auto">
+<div class="w-prose mx-auto markdown">
 	<h1 class="mt-1">Blindfolded Cube Resources</h1>
+	<ul>
+		<li>
+			<a href="/op-corners">Old Pochman Corners</a>: ?
+		</li>
+		<li>
+			<a href="/corner-drawer">Corner Drawer</a>: Generate HTML of corners for Anki
+		</li>
+		<li>
+			<a href="/anki-stuff">Anki Stuff</a>: Stuff to handle organising an Anki collection
+		</li>
+	</ul>
 </div>
