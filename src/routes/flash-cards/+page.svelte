6d00d1eb8a794cd8c6c70a2abfc84059616c6d72
Nick Ng <nick-ng@users.noreply.github.com>
2024-10-06 10:39:37 +1300

    chore: small refactor

--- src/routes/flash-cards/+page.svelte
+++ src/routes/flash-cards/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 </script>
 
-<div class="max-w-prose mx-auto text-center">
-	<h1>Flash Cards</h1>
+<div class="max-w-prose mx-auto">
+	<h1 class="text-center">Flash Cards</h1>
 	<ul>
 		<li class="mt-1">
-			<a class="text-xl leading-none py-2 button-default no-underline" href="/flash-cards/quiz"
+			<a class="text-xl leading-none py-2 button-default text-center" href="/flash-cards/quiz"
 				>Quiz</a
 			>
 		</li>
 		<li class="mt-1">
-			<a class="text-xl leading-none py-2 button-default no-underline" href="/flash-cards/edit"
+			<a class="text-xl leading-none py-2 button-default text-center" href="/flash-cards/edit"
 				>Edit</a
 			>
 		</li>
