a71179bdb2830760238be91108439905888b929b
Nick Ng <nick-ng@users.noreply.github.com>
2024-10-06 20:48:48 +1300

    chore: remove unused code from server

--- server/main.go
+++ server/main.go
@@ -3,21 +3,10 @@ package main
 import (
 	"bld-server/routes"
 	"bld-server/utils"
-	"fmt"
 	"net/http"
 )
 
 func rootHandler(writer http.ResponseWriter, req *http.Request) {
-	headerMap := map[string]string{}
-
-	for name, headers := range req.Header {
-		if len(headers) > 0 {
-			headerMap[name] = headers[len(headers)-1]
-		}
-	}
-
-	fmt.Println(headerMap["X-Pants"])
-
 	switch req.Method {
 	case "GET":
 		{
