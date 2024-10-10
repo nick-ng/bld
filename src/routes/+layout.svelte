036268391390d92b1e667cce0a163612be0524fd
Nick Ng <nick-ng@users.noreply.github.com>
2024-10-10 18:28:10 +1300

    feat: add abort controller to quiz

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,10 +10,12 @@
 </svelte:head>
 
 {#if showNav}
-	<div class="flex flex-row mx-2 my-2 gap-2">
+	<div class="flex flex-row items-start mx-2 my-2 gap-2">
 		<a class="button-default" href="/flash-cards">Flash Cards</a>
 		<a class="button-default" href="/corner-drawer">Corner Drawer</a>
 		<a class="button-default" href="/anki-stuff">Anki Stuff</a>
+		<div class="flex-grow" />
+		<div class="text-xs">v0.5</div>
 	</div>
 {/if}
 <div class={showNav ? "mx-1" : ""}>
