b14d741a895b12f133f6bf03393e88c7ad7cdc5d
nick-ng <nick-ng@users.noreply.github.com>
2025-07-12 11:07:20 +1200

    feat: show separate letter pair and parity count on quiz screen. don't show corner drawer link on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
 			href="/quiz">Quiz</a
 		>
 		<a
-			class="cannot-hover:py-2 block rounded border border-gray-600 px-2 py-0 dark:border-gray-300"
+			class="cannot-hover:py-2 hidden rounded border border-gray-600 px-2 py-0 lg:block dark:border-gray-300"
 			href="/corner-drawer">Corner Drawer</a
 		>
 	</div>
