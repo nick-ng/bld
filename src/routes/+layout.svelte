6945c142bdbec9a71ecf33adcb387c7ca1d01060
nick-ng <nick-ng@users.noreply.github.com>
2025-07-11 22:24:31 +1200

    feat: simplify nav. change where you go when you finish editing a flash card. update commutator link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,6 +23,10 @@
 		>
 		<a
 			class="cannot-hover:py-2 block rounded border border-gray-600 px-2 py-0 dark:border-gray-300"
+			href="/flash-cards/summary">Summary</a
+		>
+		<a
+			class="cannot-hover:py-2 block rounded border border-gray-600 px-2 py-0 dark:border-gray-300"
 			href="/quiz">Quiz</a
 		>
 		<a
