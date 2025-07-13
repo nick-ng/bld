617c0ea03fe21c231b366138c9f8cdddede9f953
nick-ng <nick-ng@users.noreply.github.com>
2025-07-13 12:07:03 +1200

    wip

--- src/routes/big-scramble/+page.svelte
+++ src/routes/big-scramble/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { page } from "$app/stores";
+	import Cube from "cubejs";
+	import { page } from "$app/state";
 	import ClockHand from "$lib/components/clock-hand.svelte";
 	import Timer from "./timer.svelte";
 
@@ -45,12 +46,26 @@
 	const getCubeImageUrl = (scramble: string) =>
 		`https://cube.rider.biz/visualcube.php?fmt=svg&size=265&view=plan&bg=black&dist=1.35&alg=x2${scramble.replaceAll(" ", "")}`;
 
+	const cube = new Cube();
 	let size: number = 120;
 	let fontOutline: number = 2;
 	let scramble = $state("L2 U L2 F2 R U' B' D R D' B2 U F2 D' F2 U' F2 D2 Rw Uw'");
+	// UuuuuuRrrrrrFfffffDdddddLlllllBbbbbb
+	let cubeFacesString = $derived.by(() => {
+		cube.identity();
+		const scramble2 = scramble
+			.replaceAll("Uw", "u")
+			.replaceAll("Rw", "r")
+			.replaceAll("Fw", "f")
+			.replaceAll("Dw", "d")
+			.replaceAll("Lw", "l")
+			.replaceAll("Bw", "b");
+		cube.move(scramble2);
+		return cube.asString();
+	});
 
 	onMount(() => {
-		const startingScramble = $page.url.searchParams.get("scramble");
+		const startingScramble = page.url.searchParams.get("scramble");
 
 		if (typeof startingScramble === "string") {
 			scramble = startingScramble;
@@ -73,7 +88,7 @@
 </script>
 
 <svelte:head>
-	<title>Blindfolded Cube Resources - Big Scramble</title>
+	<title>Big Scramble</title>
 </svelte:head>
 
 <div class="big-scramble-root">
@@ -115,10 +130,18 @@
 			<ClockHand isMinutes />
 		</div>
 	</div>
-	<a class="cube-preview" href="https://cube.rider.biz/visualcube.php" target="_blank">
-		<img src={getCubeImageUrl(scramble)} alt="U Face" />
-		<img src={getCubeImageUrl(`${scramble}x`)} alt="F Face" />
-	</a>
+	<div class="cube-preview text-white">
+		{cubeFacesString}
+		<div class="grid grid-cols-3 gap-1">
+			{#each cubeFacesString.slice(0, 10) as cubeFace, i (`U-${cubeFace}-${i}`)}
+				<div class={`w-10: h-10 ${cubeFace.toLowerCase()}`}>
+					{#if i === 4}
+						U
+					{/if}
+				</div>
+			{/each}
+		</div>
+	</div>
 	<div class="footer">
 		Scrambled cube images generated using Conrad Rider's <a
 			class="text-blue-500"
@@ -155,10 +178,6 @@
 		position: absolute;
 		right: 0;
 		bottom: 0;
-	}
-
-	.cube-preview img {
-		max-width: 10vw;
 	}
 
 	.footer {
