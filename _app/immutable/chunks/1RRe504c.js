ac3c0aa5bc52d92e70dbe99414b72977faf5aab6
nick-ng <nick-ng@users.noreply.github.com>
2025-07-18 10:47:11 +0000

    Fri Jul 18 10:47:11 UTC 2025 deploy front-end

---
+++ _app/immutable/chunks/1RRe504c.js
@@ -0,0 +1,1 @@
+import{ag as a}from"./BsaY4pJM.js";a();
---
+++ _app/immutable/nodes/11.C3jVZbd8.js
@@ -0,0 +1,1 @@
+import"../chunks/NZTpNUN0.js";import"../chunks/1RRe504c.js";import{f as r,b as m}from"../chunks/BsaY4pJM.js";var t=r('<div class="mx-auto max-w-[130ch]"><h1>Old Pochman Corners</h1></div>');function s(o){var a=t();m(o,a)}export{s as component};
