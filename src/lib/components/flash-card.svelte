18d375c5d84f032bb16f18e54a754ed331ea441f
nick-ng <nick-ng@users.noreply.github.com>
2025-07-18 22:44:53 +1200

    fix: don't show both inverse card and end quiz button at the same time

--- src/lib/components/flash-card.svelte
+++ src/lib/components/flash-card.svelte
@@ -30,13 +30,6 @@
 		<div class={`relative flex flex-col items-center gap-1 ${extraClass}`}>
 			<div class="relative m-0 flex flex-row justify-center self-stretch">
 				<h2 class="m-0 uppercase">{flashCard.letterPair}</h2>
-				{#if flashCard.letterPair.length === 2 && flashCard.letterPair[0] !== flashCard.letterPair[1]}
-					{@const inverseLetterPair = `${flashCard.letterPair[1]}${flashCard.letterPair[0]}`}
-					<a
-						class="absolute top-0 right-0 bottom-0 my-auto block uppercase"
-						href={`/flash-cards?t=${flashCard.type}&lp=${inverseLetterPair}`}>{inverseLetterPair}</a
-					>
-				{/if}
 			</div>
 			{#if quizLeft > 0}
 				<div class="absolute top-0 left-0 text-left">{quizLeft} left</div>
@@ -45,6 +38,12 @@
 				<button
 					class="cannot-hover:py-2 absolute top-0 right-0 block rounded border border-gray-600 px-2 py-0 dark:border-gray-300"
 					onclick={onQuizEnd}>End Quiz</button
+				>
+			{:else if flashCard.letterPair.length === 2 && flashCard.letterPair[0] !== flashCard.letterPair[1]}
+				{@const inverseLetterPair = `${flashCard.letterPair[1]}${flashCard.letterPair[0]}`}
+				<a
+					class="cannot-hover:py-2 absolute top-0 right-0 block rounded border border-gray-600 px-2 py-0 uppercase dark:border-gray-300"
+					href={`/flash-cards?t=${flashCard.type}&lp=${inverseLetterPair}`}>{inverseLetterPair}</a
 				>
 			{/if}
 			<Corners letterPair={flashCard.letterPair} />
