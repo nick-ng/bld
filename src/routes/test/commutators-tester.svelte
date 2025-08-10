43cf3a474fbb9071b30a5218e78f0423a0c75b10
nick-ng <nick-ng@users.noreply.github.com>
2025-08-10 23:11:15 +1200

    wip: added test page to test commutator stuff

---
+++ src/routes/test/commutators-tester.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { commutatorDetails } from "$lib/utils";
+	let singleCommutator = $state("");
+	let parsedCommutator = $derived(commutatorDetails(singleCommutator));
+</script>
+
+<div>
+	<table class="border-separate border-spacing-x-2">
+		<tbody>
+			<tr>
+				<td>
+					<label for="raw-single-commutator-input">Commutator</label>
+				</td>
+				<td>
+					<input id="raw-single-commutator-input" type="text" bind:value={singleCommutator} />
+				</td>
+			</tr>
+			<tr>
+				<td> Expanded Commutator </td>
+				<td>
+					{singleCommutator}
+				</td>
+			</tr>
+			<tr>
+				<td> Commutator Stats </td>
+				<td>
+					<pre>{JSON.stringify(parsedCommutator, null, "  ")}</pre>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
