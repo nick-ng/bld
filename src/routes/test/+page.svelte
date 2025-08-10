a8b43ce1c2f9aac54c0282bf7aa4b2456b111df8
nick-ng <nick-ng@users.noreply.github.com>
2025-08-10 23:11:15 +1200

    wip: added test page to test commutator stuff

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import CommutatorsTester from "./commutators-tester.svelte";
+</script>
+
+<div class="mx-2">
+	<details>
+		<summary>Commutators</summary>
+		<div>
+			<CommutatorsTester />
+		</div>
+	</details>
+</div>
