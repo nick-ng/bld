38c309a56f049c883dca5d9663eda8a76c3d1cc3
nick-ng <nick-ng@users.noreply.github.com>
2025-08-30 17:38:30 +1200

    feat: max quiz age is quiz cooldown

--- src/lib/stores/quiz.ts
+++ src/lib/stores/quiz.ts
@@ -1,10 +1,11 @@
-import { writable } from "svelte/store";
+import { get, writable } from "svelte/store";
 import { browser } from "$app/environment";
 import {
 	CURRENT_QUIZ_LETTER_PAIRS_STORE_KEY,
 	CURRENT_QUIZ_AGE_STORE_KEY,
 	CURRENT_QUIZ_TYPE_STORE_KEY
 } from "$lib/constants";
+import { optionsStore } from "$lib/stores/options";
 
 export const quizStore = writable<string[]>([]);
 export const quizTypeStore = writable<string>("normal");
@@ -13,7 +14,6 @@ export const touchCurrentQuiz = () => {
 	localStorage.setItem(CURRENT_QUIZ_AGE_STORE_KEY, Date.now().toString());
 };
 
-const MAX_QUIZ_AGE_MS = 1000 * 60 * 60; // 1 hour
 const loadQuiz = () => {
 	try {
 		const lastQuizMsString = localStorage.getItem(CURRENT_QUIZ_AGE_STORE_KEY);
@@ -22,7 +22,8 @@ const loadQuiz = () => {
 		}
 
 		const lastQuizMs = parseInt(lastQuizMsString, 10);
-		if (Date.now() - lastQuizMs > MAX_QUIZ_AGE_MS) {
+		const maxQuizAgeMs = get(optionsStore).leitnerQuizCooldownHours * 60 * 60 * 1000;
+		if (Date.now() - lastQuizMs > maxQuizAgeMs) {
 			return;
 		}
 
