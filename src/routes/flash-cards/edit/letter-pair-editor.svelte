ae5c11ce79fd476496beec611eaa49b2d9aca0a7
nick-ng <nick-ng@users.noreply.github.com>
2025-09-08 00:13:46 +1200

    fix: remove unused import

--- src/routes/flash-cards/edit/letter-pair-editor.svelte
+++ src/routes/flash-cards/edit/letter-pair-editor.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { parseFlashCard, defaultFlashCard } from "$lib/types";
+	import { parseFlashCard } from "$lib/types";
 	import {
 		joinServerPath,
 		upperCaseFirst,
