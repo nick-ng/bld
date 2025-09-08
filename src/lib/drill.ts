cab4226f07c48b0eac9d2327ce2b4f5a7edc28b7
nick-ng <nick-ng@users.noreply.github.com>
2025-09-08 23:20:15 +1200

    feat: save drill results

--- src/lib/drill.ts
+++ src/lib/drill.ts
@@ -1,14 +1,15 @@
 import z from "zod";
 import { get } from "svelte/store";
 import { DRILL_ITEMS_STORE_KEY } from "$lib/constants";
-import { fetchFlashCards, flashCardStore } from "$lib/stores/flash-cards";
+import { fetchFlashCards, flashCardStore, getAllFlashCardsOfType } from "$lib/stores/flash-cards";
 import { shuffleArray } from "$lib/utils";
 
 const drillItemSchema = z.object({
 	letterPair: z.string(),
+	flashCardType: z.string(),
 	quizzed: z.boolean(),
-	timeMs: z.number(),
-	skipped: z.boolean()
+	send: z.boolean(),
+	timeMs: z.number()
 });
 
 export type DrillItem = z.infer<typeof drillItemSchema>;
@@ -62,6 +63,12 @@ const letters = [
 ];
 
 export const drillSets = [
+	{
+		type: "random",
+		key: "random-5",
+		value: 5,
+		label: "Random 5"
+	},
 	...letters.map((l) => ({
 		type: "starts-with",
 		key: `$starts-with-${l}`,
@@ -70,29 +77,50 @@ export const drillSets = [
 	}))
 ];
 
-export const makeDrillSet = async (key: string): Promise<DrillItem[]> => {
+export const makeDrillSet = async (key: string, flashCardType: string): Promise<DrillItem[]> => {
 	const drillSet = drillSets.find((s) => s.key === key);
 	if (!drillSet) {
 		return [];
 	}
 
 	await fetchFlashCards();
-	const flashCards = get(flashCardStore);
+	const flashCards = shuffleArray(getAllFlashCardsOfType(flashCardType, get(flashCardStore)));
 
+	const value = drillSet.value;
 	switch (drillSet.type) {
+		case "random": {
+			if (typeof value !== "number") {
+				console.error("random value must be number");
+
+				return [];
+			}
+
+			return flashCards.slice(0, value).map((fc) => ({
+				letterPair: fc.letterPair,
+				flashCardType,
+				quizzed: false,
+				send: true,
+				timeMs: 25_500 // 255 tenths of a second in milliseconds
+			}));
+		}
 		case "starts-with": {
-			const matchingFlashCards = Object.values(flashCards).filter((f) =>
-				f.letterPair.toLowerCase().startsWith(drillSet.value.toLowerCase())
+			if (typeof value !== "string") {
+				console.error("starts-with value must be string");
+
+				return [];
+			}
+
+			const matchingFlashCards = flashCards.filter((f) =>
+				f.letterPair.toLowerCase().startsWith(value.toLowerCase())
 			);
 
-			const newDrill = shuffleArray(
-				matchingFlashCards.map((fc) => ({
-					letterPair: fc.letterPair,
-					quizzed: false,
-					timeMs: 25_500, // 255 tenths of a second in milliseconds
-					skipped: true
-				}))
-			);
+			const newDrill = matchingFlashCards.map((fc) => ({
+				letterPair: fc.letterPair,
+				flashCardType,
+				quizzed: false,
+				send: true,
+				timeMs: 25_500 // 255 tenths of a second in milliseconds
+			}));
 
 			localStorage.setItem(DRILL_ITEMS_STORE_KEY, JSON.stringify(newDrill));
 
