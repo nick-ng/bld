dbd044e90e6f2daaec34233de0cc25d4a9a788d4
pux <nick-ng@users.noreply.github.com>
2026-02-07 19:54:32 +1300

    feat: made youtube player

--- src/routes/mbld/analyse/+page.svelte
+++ src/routes/mbld/analyse/+page.svelte
@@ -1,50 +1,79 @@
 <script lang="ts">
-	import { randomScrambleForEvent } from "cubing/scramble";
-	import CubeFace from "$lib/components/cube-face.svelte";
+	import Youtube from "$lib/components/youtube.svelte";
 	import { mbldStore } from "$lib/stores/mbld";
+	import { getVideoId, hhmmssToSeconds, secondsToHhmmss, formatDate } from "$lib/utils";
+	import MbldCube from "./mbld-cube.svelte";
 
 	let message = $state("");
-	let selectedAttemptIndex = $state(-1);
-	let selectedAttempt = $derived($mbldStore[selectedAttemptIndex - 1]);
-
-	let generatedScrambles: string[] = $state([]);
-	// @todo(nick-ng): shorten date in <select>
+	let selectValue = $state(-1);
+	let selectedIndex = $derived(selectValue - 1);
+	let player: any = $state(null);
 </script>
 
 <div>
 	<div class="space-between mb-1 flex flex-row gap-1">
-		<select class="px-1" bind:value={selectedAttemptIndex}>
+		<select class="px-1" bind:value={selectValue}>
 			<option value={-1}>New</option>
 			{#each $mbldStore.slice(-20) as mbldAttempt, i (mbldAttempt.date)}
-				<option value={i + 1}>{mbldAttempt.date}</option>
+				<option value={i + 1}>{formatDate(mbldAttempt.date)}</option>
 			{/each}
 		</select>
 		<button
-			disabled={!selectedAttempt}
+			disabled={!$mbldStore[selectedIndex]}
 			onclick={async () => {
 				message = "Please wait";
-				await navigator.clipboard.writeText(selectedAttempt?.scrambles.join("\n"));
-				message = `${selectedAttempt?.scrambles.length} copied to clipboard`;
+				await navigator.clipboard.writeText($mbldStore[selectedIndex]?.scrambles.join("\n"));
+				message = `${$mbldStore[selectedIndex]?.scrambles.length} copied to clipboard`;
 			}}>Copy Scrambles</button
 		>
 		<button
-			disabled={!selectedAttempt}
+			disabled={!$mbldStore[selectedIndex]}
 			onclick={async () => {
 				message = "Please wait";
-				await navigator.clipboard.writeText(JSON.stringify(selectedAttempt, null, 2));
+				await navigator.clipboard.writeText(JSON.stringify($mbldStore[selectedIndex], null, 2));
 				message = `Attempt copied to clipboard`;
 			}}>Copy Attempt</button
 		>
 		<div>{message}</div>
 	</div>
-	{#if selectedAttempt}
+	{#if $mbldStore[selectedIndex]}
 		<div class="flex flex-row justify-start gap-2">
 			<div>
-				{#each selectedAttempt.cubes as cube, i (`${i}-cube`)}
-					<div>
-						<pre>{JSON.stringify(cube, null, 2)}</pre>
-					</div>
+				<table class="border-separate border-spacing-2">
+					<tbody>
+						<tr>
+							<td>Date</td>
+							<td>{formatDate($mbldStore[selectedIndex].date)}</td>
+						</tr>
+						<tr>
+							<td>YouTube Link</td>
+							<td>
+								<input type="text" bind:value={$mbldStore[selectedIndex].youtube_link} />
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				{#each $mbldStore[selectedIndex].cubes as cube, i (`${i}-cube`)}
+					<MbldCube
+						{cube}
+						onSave={(newCube) => {
+							console.log("new cube", newCube);
+						}}
+						onSeekRequest={(newSec) => {
+							console.log("newSec", newSec);
+						}}
+					/>
 				{/each}
+			</div>
+			<div class="max-h-[70vh] grow">
+				{#if $mbldStore[selectedIndex]?.youtube_link}
+					{#key $mbldStore[selectedIndex]?.youtube_link}
+						<Youtube
+							bind:player
+							initialVideoId={getVideoId($mbldStore[selectedIndex]?.youtube_link || "").id}
+						/>
+					{/key}
+				{/if}
 			</div>
 		</div>
 	{/if}
---
+++ src/routes/mbld/analyse/mbld-cube.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { MbldCube } from "$lib/types";
+
+	interface Props {
+		cube: MbldCube;
+		onSeekRequest: (sec: number) => Promise<void> | void;
+		onSave: (newCube: MbldCube) => Promise<void> | void;
+	}
+
+	let { cube }: Props = $props();
+</script>
+
+<div>
+	<pre>{JSON.stringify(cube, null, 2)}</pre>
+</div>
