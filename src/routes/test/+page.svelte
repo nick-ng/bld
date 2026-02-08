65638141d6e5ac3feeb46c6845d2401fd71d4211
pux <nick-ng@users.noreply.github.com>
2026-02-08 22:56:06 +1300

    wip

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
 	import CommutatorsTester from "./commutators-tester.svelte";
+
+	let videoUrl = $state("");
+	let videoHtml: HTMLVideoElement | null = $state(null);
 </script>
 
 <div class="mx-2">
+	<details>
+		<summary>Video</summary>
+		<input type="text" bind:value={videoUrl} />
+		<button
+			onclick={() => {
+				if (videoHtml) {
+					videoHtml.currentTime = 120;
+				}
+			}}>Skip to 2:00</button
+		>
+		<video src={videoUrl} controls bind:this={videoHtml}></video>
+	</details>
 	<details>
 		<summary>Commutators</summary>
 		<div>
